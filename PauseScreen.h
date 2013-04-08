455f57ee2dfa3e79f3e7613487272f42d48b007f
Charles Marlon Ramones <charles@klab.com>
2013-04-08 09:30:39 -0700

    monday 4/8/2013

---
+++ PauseScreen.h
@@ -0,0 +1,13 @@
+//
+//  PauseScreen.h
+//  BreakoutClone
+//
+//  Created by Charles Marlon G. Ramones on 4/8/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PauseScreen : NSObject
+
+@end
---
+++ PauseScreen.m
@@ -0,0 +1,13 @@
+//
+//  PauseScreen.m
+//  BreakoutClone
+//
+//  Created by Charles Marlon G. Ramones on 4/8/13.
+//
+//
+
+#import "PauseScreen.h"
+
+@implementation PauseScreen
+
+@end
